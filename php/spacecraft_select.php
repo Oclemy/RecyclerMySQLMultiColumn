7f02ee7de2d668c482e4744c384227d5d129d38a
Oclemy <oclemmi@gmail.com>
2016-11-18 20:34:36 -0800

    Added PHP file
    PHP code for MySQl data retrieval added.

---
+++ php/spacecraft_select.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$host='127.0.0.1';
+$username='root';
+$pwd='';
+$db="spacecraftDB";
+
+$con=mysqli_connect($host,$username,$pwd,$db) or die('Unable to connect');
+
+if(mysqli_connect_error($con))
+{
+	echo "Failed to Connect to Database ".mysqli_connect_error();
+}
+
+
+$sql="SELECT * FROM spacecraftTB";
+
+
+$result=mysqli_query($con,$sql);
+if($result)
+{
+	while($row=mysqli_fetch_array($result))
+	{
+		$flag[]=$row;
+	}
+	
+	print(json_encode($flag));
+	
+}
+
+mysqli_close($con);
+
+?>
